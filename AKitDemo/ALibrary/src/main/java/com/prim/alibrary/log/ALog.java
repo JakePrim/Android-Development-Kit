a1d25933d78c2c9b53bd720199ad8881a6694144
sfl-jakeprim <sufulu@foxmail.com>
2020-05-31 16:45:48 +0800

    init

---
+++ AKitDemo/ALibrary/src/main/java/com/prim/alibrary/log/ALog.java
@@ -0,0 +1,12 @@
+package com.prim.alibrary.log;
+
+/**
+ * @author prim
+ * @version 1.0.0
+ * @desc Log 相关的配置
+ * @time 2020/5/31 - 4:43 PM
+ * @contact https://jakeprim.cn
+ * @name AKitDemo
+ */
+public class ALog {
+}
